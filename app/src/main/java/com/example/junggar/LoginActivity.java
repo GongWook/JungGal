b63d789a3ab548968f96b53e81e584cfa2e530d1
GongWook <72791182+GongWook@users.noreply.github.com>
2021-04-25 17:40:52 +0900

    회원가입 ui 개발 완료 기종 별 밑에 화면 잘림 수정 필요 Firebase 회원가입 연동 필요

--- app/src/main/java/com/example/junggar/LoginActivity.java
+++ app/src/main/java/com/example/junggar/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.junggar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
+        TextView registerbutton = findViewById(R.id.register_button);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 

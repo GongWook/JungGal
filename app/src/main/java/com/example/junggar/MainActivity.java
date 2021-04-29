f1a6889f1f0f8bfde26ccf933db48fe985245f19
집 <bboqq1@naver.com>
2021-04-29 12:20:29 +0900

    로그인 및 회원가입 (수정중)

--- app/src/main/java/com/example/junggar/MainActivity.java
+++ app/src/main/java/com/example/junggar/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.junggar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText Edit_id;
+    private EditText Edit_nickname;
+    private EditText Edit_password;
+    private EditText Edit_passwordchk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        Edit_id = (EditText)findViewById(R.id.Edit_id);
+        Edit_password = (EditText)findViewById(R.id.Edit_password);
+
+        Button Btn_register = (Button) findViewById(R.id.Btn_register);
+        Btn_register.setOnClickListener(view -> {
+            createUser(Edit_id.getText().toString(), Edit_password.getText().toString());
+        });
+
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                        } else {
+                            Toast.makeText(MainActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }

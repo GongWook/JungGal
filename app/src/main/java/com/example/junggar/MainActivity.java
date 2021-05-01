f2a3d963f851b465eca5b7073f6ca71d3fee4ff0
집 <bboqq1@naver.com>
2021-05-01 19:12:58 +0900

    Quiz UI, 글쓰기 UI 외 이미지파일 추가

--- app/src/main/java/com/example/junggar/MainActivity.java
+++ app/src/main/java/com/example/junggar/MainActivity.java
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -20,35 +17,28 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private EditText Edit_id;
-    private EditText Edit_nickname;
-    private EditText Edit_password;
-    private EditText Edit_passwordchk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        Edit_id = (EditText)findViewById(R.id.Edit_id);
-        Edit_password = (EditText)findViewById(R.id.Edit_password);
-
-        Button Btn_register = (Button) findViewById(R.id.Btn_register);
-        Btn_register.setOnClickListener(view -> {
-            createUser(Edit_id.getText().toString(), Edit_password.getText().toString());
-        });
+        // 로그인이랑 회원가입 연동기능 때매 조금 건드렸어요 불필요한내용 지우셔도됩니다! 여긴 테스트해보고 다시 작성해볼게요 -동욱
 
     }
-
-    private void createUser(String email, String password) {
+    }
+    
+    /* private createUser(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                         } else {
-                            Toast.makeText(MainActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
+
                         }
                     }
                 });
-    }
-}
+    } */

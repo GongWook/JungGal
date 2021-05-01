1a403ae0aaab8d70709e6284743191bc7f6567fc
GongWook <72791182+GongWook@users.noreply.github.com>
2021-05-02 01:34:28 +0900

    어플 초기화 및 최적화 (recycler java 파일 제거 완료) 블로그 recycler dependencies (github 파일 제거)

--- app/src/main/java/com/example/junggar/LoginActivity.java
+++ app/src/main/java/com/example/junggar/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.junggar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,10 +16,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         TextView registerbutton = findViewById(R.id.register_button);
+        Button tmp_btn = findViewById(R.id.Btn_tmp);
+
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tmp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SearchActivity.class );
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/junggar/SearchActivity.java
+++ app/src/main/java/com/example/junggar/SearchActivity.java
@@ -1,67 +1,14 @@
 package com.example.junggar;
 
-/*import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.util.Log;
 
-//import java.util.Arrays;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;//
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<Contents> arrayList;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        recyclerView =findViewById(R.id.recyclerview);
-        recyclerView.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        arrayList=new ArrayList<>();
-
-        database=FirebaseDatabase.getInstance();
-
-        databaseReference=database.getReference("Contents");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                arrayList.clear();
-                for(DataSnapshot snapshot1 : snapshot.getChildren()){
-                    Contents contents = snapshot.getValue(Contents.class);
-                    arrayList.add(contents);
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("RecyclerActivity", String.valueOf(error.toException()));
-            }
-        });
-
-        adapter=new CustomAdapter(arrayList,this);
-        recyclerView.setAdapter(adapter);
-
-
-
     }
-}*/
+}

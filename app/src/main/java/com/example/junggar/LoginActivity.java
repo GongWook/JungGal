87a60af63cc6da96544cb8189d9cf40a473e3bc4
GongWook <72791182+GongWook@users.noreply.github.com>
2021-05-02 16:45:06 +0900

    로그인 시 지도 객체 생성 완료 (회원가입도 가능)

--- app/src/main/java/com/example/junggar/LoginActivity.java
+++ app/src/main/java/com/example/junggar/LoginActivity.java
@@ -4,12 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.internal.InternalTokenProvider;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+    private EditText email_login;
+    private EditText pwd_login;
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +33,11 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView registerbutton = findViewById(R.id.register_button);
         Button tmp_btn = findViewById(R.id.Btn_tmp);
+
+        email_login = (EditText) findViewById(R.id.Login_id);
+        pwd_login = (EditText) findViewById(R.id.Login_password);
+        login = (Button) findViewById(R.id.Btn_login);
+        firebaseAuth = firebaseAuth.getInstance(); //firebaseAuth의 인스턴스르 가져온다.
 
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +52,28 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SearchActivity.class );
                 startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_login.getText().toString().trim();
+                String pwd = pwd_login.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //성공했을 때
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            //실패했을 때
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
